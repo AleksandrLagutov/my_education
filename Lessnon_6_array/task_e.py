aeba5034d469070e9c026ee1586178749a06348d
Elllesar <lagutov.aleksandr@gmail.com>
2021-03-24 21:49:09 +0300

    Реализовал сортировку элементов массива. Осталось оформить вывод результата, исключить крайние случаи на вводе данных.

--- Lessnon_6_array/task_e.py
+++ Lessnon_6_array/task_e.py
@@ -3,22 +3,33 @@ array_students = [0]*amount_students  # Список результатов ст
 amount_result_test = [0]*amount_students # Список количества тестов пройденных каждым студентом
 input_data = input()
 
-def create_array_resalt(amount):
-    for i in range(amount):
-        student_i = 0
-    pass
 
-def update_result_students(id, result, amount_resalt):
+
+
+def sort_on_summ_bals(element):
+    """
+    Функция возвращает сумму элкментов в списке, если на вход подается одно число то возвращает его
+    :param element:
+    :return:
+    """
+    if type(element) != int:
+        sum_elements = sum(element)
+    else:
+        sum_elements = element
+    return sum_elements
+
+
+def update_result_students(id, result, amount_result):
     """
 Функция принимает ID студента и результат, и количество результатов студента
 Должна создать массив в ячейке array_students[id] длиноой количество результатов перед эти сохранива старый и добавив
 новый элемент от отсортировать
     """
-    old_result = [0]*amount_resalt
-    new_result = [0]*amount_resalt
-    for i in range(amount_resalt-1):
+    old_result = [0]*amount_result
+    new_result = [0]*amount_result
+    for i in range(amount_result-1):
         old_result[i] = array_students[id][i]       # запоминаем прошлые результаты
-    old_result[amount_resalt-1] = result
+    old_result[amount_result-1] = result
     array_students[id] = sorted(old_result, reverse=True)
 
 
@@ -27,11 +38,15 @@ def update_result_students(id, result, amount_resalt):
 while input_data != '#':
     result_of_test = input_data.split()
     id_student = int(result_of_test[0])
-    amount_result_test[id_student] +=1
-    update_result_students(id_student, int(result_of_test[1]), int(amount_result_test[id_student] ))
+    amount_result_test[id_student] += 1
+    update_result_students(id_student, int(result_of_test[1]), int(amount_result_test[id_student]))
 
     print(array_students)
-    print(amount_result_test)
+
 
 
     input_data = input()
+sorted_array_students = sorted(array_students, key=lambda x: sort_on_summ_bals(x), reverse=True)
+print(sorted_array_students)
+
+
