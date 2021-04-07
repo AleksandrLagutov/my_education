c1b5dd05fa65122113d7edc59f75d6d994147b27
Elllesar <lagutov.aleksandr@gmail.com>
2021-04-07 21:55:32 +0300

    Решил через циклы, нужно реализовать через динамическое программирование

--- lesson_11_dinamic_programming/exercise_5.py
+++ lesson_11_dinamic_programming/exercise_5.py
@@ -1,18 +1,18 @@
 
 N = 6
-M = 6
-ferz_pos = 0, 1
-K = [[0]*(M+1) for i in range(N+1)]
-K[0][0] =0
-for i in range(1, N+1):
-    for j in range(1, M+1):
+M = 10
+K = [[0]*(M) for i in range(N)]
+K[0][0] = 0
+for i in range(0, N):
+    for j in range(0, M):
+        test = K[i][j]
         if K[i][j] == 0:
-            K[i+1][j] =1 ; K[i][j+1] = 1; K[i+1][j+1] = 1
-        elif K[i-1][j] == 1 and K[i][j-1] == 1 and K[i-1][j-1] == 1:
-            K[i][j] = 0
-
-
-
+            for k in range(i+1, N):
+                K[k][j] = 1
+                for t in range(j+1, M):
+                    K[i][t] = 1
+                    if abs(i - k) == abs(j - t):
+                        K[k][t] = 1
 
 for i in range(N):
     print(K[i])
