b700f92dc26e0ba3ff80a48dd811ecc51aa60f93
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-10 13:19:46 +0300

    Задача A-Два хода конем

--- lesson_12_contest_dinamic_programming/test_a.py
+++ lesson_12_contest_dinamic_programming/test_a.py
@@ -10,3 +10,24 @@
 Формат выходных данных
 Одно число — количество ходов, за которые можно попасть из из одной клетки во вторую. Если невозможно -- вывести -1.
 """
+
+x1, y1, x2, y2 = [int(input()) for i in range(4)]
+
+flag = True
+if not (x1 == x2 and y1 == y2):
+    for i in range(1, 9):
+        for j in range(1, 9):
+            if flag and (abs(x1-i) == 2 and abs(y1-j) == 1 or abs(x1-i) == 1 and abs(y1-j) == 2):
+                if i == x2 and j == y2:
+                    print(1)
+                    flag = False
+                    break
+                elif abs(x2 - i) == 2 and abs(y2 - j) == 1 or abs(x2 - i) == 1 and abs(y2 - j) == 2:
+                    print(2)
+                    flag = False
+                    break
+            elif i == 8 and j == 8 and flag:
+                print(-1)
+else:
+    print(0)
+
