c98a476b900207211e06aa51dcc2725c45944c56
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-15 16:12:03 +0300

    Упражнение №1

---
+++ lesson_14_arifmetics/exercise_1_ariphetics.py
@@ -0,0 +1,49 @@
+"""
+>>> is_empty_stack()
+True
+>>> push_steck(5)
+>>> is_empty_stack()
+False
+>>> pop_steck()
+5
+>>> is_empty_stack()
+True
+"""
+steck = []
+
+
+def push_steck(x):
+    """
+    Функция кладет в стек переданное значение
+    :return:
+    """
+    steck.append(x)
+
+def pop_steck():
+    """
+    Функия забирает крайний элемент из стека и респечатывает его
+    :return:
+    """
+    x = steck.pop()
+    return x
+
+def is_empty_stack():
+    """
+    Функция проверяет пустой ли стек, если  пустой возвращает True
+    :return:
+    """
+    if len(steck) == 0:
+        return True
+    return False
+
+
+def main():
+    print('Всё круто')
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    main()
+
