9535b749b5936bb44dda08add3a1a33c052adaf9
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-15 16:11:32 +0300

    Упражнение №3: Количество разных подстрок

--- lesson_13_z_fumction_and_KMP/3_Could_substring.py
+++ lesson_13_z_fumction_and_KMP/3_Could_substring.py
@@ -1,22 +1,23 @@
 def z_func(s):
     """
-    Эффективный алгоритм вычисления Z-функции
+    Упражнение №3: Количество разных подстрок
     :param s:
     :return:
     """
     n = len(s)
     z = [0] * n
+    k = 0 # текущее количество подстрок строки S
     l = 0
     r = 0
-    for i in range(1, n):
-        if i <= r:
-            z[i] = min(r-i+1, z[i-l])
-        elif i > r:
-            while i + z[i] < n and s[z[i]] == s[i + z[i]]:
-                z[i] += 1
-        elif i + z[i] - 1 > r:
-            l = i
-            r = i = i + z[i] - 1
+    t = ''
+    for j in range(n):
+        t += s[j]
+        rt = t[::-1]
+        for i in range(1, len(rt)):
+            while i + z[i] < len(rt) and rt[z[i]] == rt[i + z[i]]:
+                    z[i] += 1
+        k += len(rt) - max(z)
+    return k
 
 
-print(z_func('aaaabaa'))
+print(z_func('aaaaa'))
