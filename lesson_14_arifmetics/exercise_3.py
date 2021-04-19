3b5a15640404c2549113e56046f5ca8a544d9968
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-19 14:46:19 +0300

    Добавил функцию перевода в прямую полькую запись

--- lesson_14_arifmetics/exercise_3.py
+++ lesson_14_arifmetics/exercise_3.py
@@ -1,6 +1,6 @@
 """
-forward_writing('(2−3)*(12−10)+4/2')
-+∗−23−1210/42
+>>> forward_writing('(2-3)*(12-10)+4/2')
+'+*-23-1210/42'
 >>> bforwaed_writing('(2-3)*(12-10)+4/2')
 '23-1210-*42/+'
 """
@@ -18,10 +18,32 @@ def check_brace(data:str):
 
 def forward_writing(data):
     s.clear_steck()
+    priority = {'': 0, ')': 0, '+': 1, '-': 1, '*': 2, '/': 2, '^': 4}
+    p_string = []
+    for i in reversed(data):
+        if i not in '(,)+,-,*,/,^':
+            p_string.append(i)
+        elif i == ')':
+            s.push_steck(i)
+        elif i == '(':
+            temp = s.last_steck()
+            while temp != ')':
+                if not s.is_empty_stack():
+                    p_string.append(s.pop_steck())
+                    temp = s.last_steck()
+                else:
+                    return "Ошибка скобочной структуры"
+            s.pop_steck()
+        else:
+            if not s.is_empty_stack() and priority[s.last_steck()] >= priority[i]:
+                while not s.is_empty_stack():
+                    p_string.append(s.pop_steck())
+            s.push_steck(i)
+    while not s.is_empty_stack():
+        p_string.append(s.pop_steck())
+    result = ''.join(p_string)
+    return result[::-1]
 
-
-
-    pass
 
 
 def bforwaed_writing(data):
@@ -53,7 +75,7 @@ def bforwaed_writing(data):
     return result
 
 def main():
-    print(bforwaed_writing('(2-3)*(12-10)+4/2'))
+    print(forward_writing('(2-3)*(12-10)+4/2'))
 
 if __name__ == '__main__':
     main()
