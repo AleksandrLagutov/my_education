15504db131561851a116c2dadda37d3a7538355b
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-28 16:05:10 +0300

    Упражнение №5. Перевод текста Реализовал перевод текста с выводом перевода в файл

--- lesson_16_assimptotic/exercise_5.py
+++ lesson_16_assimptotic/exercise_5.py
@@ -26,17 +26,18 @@ with open("en-ru", 'r', encoding='utf8', ) as dictionary:
     data = dictionary.read()
     data_2 = data.split('\n')
     line = dict([i for i in t.split('\t-\t')] for t in data_2)
-
+out_txt = open('output.txt', 'w', encoding='utf8')
 with open("input", 'r', encoding='utf8') as sometext:
     for lines in sometext:
         stroka = lines.split()
+        temp_lines = ''
         for world in stroka:
            if line.get(world.lower().strip(',.')) != None:
-               print(line[world.lower().strip(',.')], end=' ')
+               temp_lines += line[world.lower().strip(',.')] + ' '
            else:
-               print(world, end=' ')
-        print()
-
+               temp_lines += world + ' '
+        out_txt.write(temp_lines + '\n')
+out_txt.close()
 
 
 #print(line['cat'])
