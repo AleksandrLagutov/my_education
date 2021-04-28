304cdd0ec7eb9ed8228900a9eb109cc1680075a7
Lagutov <lagutov.aleksandr@gmail.com>
2021-04-28 15:49:00 +0300

    Упражнение №5. Перевод текста Реализовал перевод текста с выводом перевода в консоль.

--- lesson_16_assimptotic/exercise_5.py
+++ lesson_16_assimptotic/exercise_5.py
@@ -21,3 +21,22 @@ Dog eats mouse too.
 Требуется сделать подстрочный перевод с помощью имеющегося словаря и вывести результат в output.txt. Незнакомые словарю
 слова нужно оставлять в исходном виде.
 """
+lint = []
+with open("en-ru", 'r', encoding='utf8', ) as dictionary:
+    data = dictionary.read()
+    data_2 = data.split('\n')
+    line = dict([i for i in t.split('\t-\t')] for t in data_2)
+
+with open("input", 'r', encoding='utf8') as sometext:
+    for lines in sometext:
+        stroka = lines.split()
+        for world in stroka:
+           if line.get(world.lower().strip(',.')) != None:
+               print(line[world.lower().strip(',.')], end=' ')
+           else:
+               print(world, end=' ')
+        print()
+
+
+
+#print(line['cat'])
