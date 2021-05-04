a395cbffd00b5af48e9f2eace70b6fb45f9f2585
Lagutov <lagutov.aleksandr@gmail.com>
2021-05-04 12:25:35 +0300

    Упражнение №1 Добавил описание программы.

--- lesson_18_pygame_and_balls/exercise_1.py
+++ lesson_18_pygame_and_balls/exercise_1.py
@@ -1,5 +1,11 @@
+"""
+Программа моделеирования шара в среде. Шар отталкивается от стен и других шаров,
+Шары которые добовляются по ажатию на девую клавишу мыши не подвижны.
+Шар который двигается, постепенно останавливается под действие силы трения.
+Шару можно придать ускорение зажав соответсвующие клетки срелки на клавиатуре
+Цвет шара изменяется в зависимости от его скорости
+"""
 import sys
-import math
 import pygame
 
 pygame.init()
@@ -14,7 +20,7 @@ x = 21
 y = 21
 vx = 20
 vy = 50
-list_of_bals = []
+list_of_balls = []
 
 
 while True:
@@ -25,7 +31,7 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                list_of_bals.append(event.pos)
+                list_of_balls.append(event.pos)
     if pygame.key.get_pressed()[pygame.K_DOWN]:
         vy += 1
     if pygame.key.get_pressed()[pygame.K_UP]:
@@ -42,7 +48,7 @@ while True:
         vx = -vx
     if int(y) + 20 >= height or int(y) - 20 <= 0:
         vy = -vy
-    for pos in list_of_bals:
+    for pos in list_of_balls:
         if ((x - pos[0])**2 + (y - pos[1])**2)**0.5 < 40: # условие пересечения колизий https://en.wikipedia.org/wiki/Elastic_collision
             dist = ((x - pos[0])**2 + (y - pos[1])**2)**0.5
             n = (pos[0] - x), (pos[1] - y)
@@ -53,10 +59,9 @@ while True:
             d2 = vx*p1*2 + vy*p3*2
             vx -= d1
             vy -= d2
-            #sys.exit()
-    window.fill((0,0,0))
-    for pos in list_of_bals:
+    window.fill((0, 0, 0))
+    for pos in list_of_balls:
         pygame.draw.circle(window, (225, 150, 200), pos, 20)
-    pygame.draw.circle(window, (abs(vy)+100,0 , abs(vx)+100), (int(x), int(y)), 20)
+    pygame.draw.circle(window, (abs(vy)+100, 0, abs(vx)+100), (int(x), int(y)), 20)
 
     pygame.display.flip()
