10365ebc15a2f8045134e507a2cddf5148a1a46c
al <lagutov.aleksandr@gmail.com>
2021-05-19 14:44:51 +0300

    2.2.1

--- Stepik Python/Basics and application/2.2.1.py
+++ Stepik Python/Basics and application/2.2.1.py
@@ -2,5 +2,14 @@ import simplecrypt
 inf_list = []
 with open('passwords.txt') as inf:
     for line in inf:
-        inf_list.append(line)
+        inf_list.append(line.strip())
 print(inf_list)
+with open('encrypted.bin', 'rb') as inp:
+    encrypted = inp.read()
+password = []
+for pas in inf_list:
+    try:
+        txt = simplecrypt.decrypt(pas, encrypted)
+        print(txt)
+    except simplecrypt.DecryptionException:
+        pass
