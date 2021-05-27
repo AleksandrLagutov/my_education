564aba518c9a8b2e0523fb5fe3261ffba4836c94
Lagutov <lagutov.aleksandr@gmail.com>
2021-05-27 16:31:56 +0300

    Добавил решение задачи с курса.

--- Stepik Python/Basics and application/3.3 Request/3.3.2.py
+++ Stepik Python/Basics and application/3.3 Request/3.3.2.py
@@ -1,10 +1,14 @@
 import re
 import requests
 
-pattern = r'<a[^>]*?href="(.*?\.)"[^>]*?>'
+pattern = r'<a[^>]*?href=[\'"]([\w*?://]*[\w*\-\w*\.*]+)'
 list_url = []
 with open('inf.html', 'r') as inpf:
     text_html = inpf.read()
     print(text_html)
     url = re.findall(pattern, text_html)
 print(url)
+
+#Login<a href="http://steeeeeeepic.org/courses">
+https://regex101.com/r/gFBb8y/17
+<a.+href=[\"']https://|ftp://|([\w*\.\w*]+)['\"].*>
