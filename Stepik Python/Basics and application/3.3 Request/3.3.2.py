dffb5cce9e962304085b412eb8d4c6c8fb6d7b7d
Lagutov <lagutov.aleksandr@gmail.com>
2021-05-28 09:19:09 +0300

    Добавил решение задачи с курса.

--- Stepik Python/Basics and application/3.3 Request/3.3.2.py
+++ Stepik Python/Basics and application/3.3 Request/3.3.2.py
@@ -1,13 +1,12 @@
 import re
 import requests
-inp_url = "http://pastebin.com/raw/hfMThaGb"
+inp_url = input('Input URL\n')
 pattern = r"<a.*?href[ ]?=['\"](?:http[s]?://|ftp://|)([^.](?:\w*[-.]+\w+)+)"
 res = requests.get(inp_url)
-print(res.status_code)
+
 inp_text = res.text
 #with open('inf.html', 'r') as inpf:
     #text_html = inpf.read()
-print(inp_text)
 url = re.findall(pattern, inp_text)
 url = list(set(url))
 
@@ -15,4 +14,3 @@ url.sort()
 for i in url:
     print(i)
 
-#Login<a href="http://steeeeeeepic.org/courses">
