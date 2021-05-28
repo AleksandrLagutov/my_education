a75899d75be608d1b0a77acfdea14ecd4233823e
Lagutov <lagutov.aleksandr@gmail.com>
2021-05-28 09:04:12 +0300

    Добавил решение задачи с курса.

--- Stepik Python/Basics and application/3.3 Request/3.3.2.py
+++ Stepik Python/Basics and application/3.3 Request/3.3.2.py
@@ -1,14 +1,18 @@
 import re
 import requests
+inp_url = "http://pastebin.com/raw/hfMThaGb"
+pattern = r"<a.*?href[ ]?=['\"](?:http[s]?://|ftp://|)([^.](?:\w*[-.]+\w+)+)"
+res = requests.get(inp_url)
+print(res.status_code)
+inp_text = res.text
+#with open('inf.html', 'r') as inpf:
+    #text_html = inpf.read()
+print(inp_text)
+url = re.findall(pattern, inp_text)
+url = list(set(url))
 
-pattern = r'<a[^>]*?href=[\'"]([\w*?://]*[\w*\-\w*\.*]+)'
-list_url = []
-with open('inf.html', 'r') as inpf:
-    text_html = inpf.read()
-    print(text_html)
-    url = re.findall(pattern, text_html)
-print(url)
+url.sort()
+for i in url:
+    print(i)
 
 #Login<a href="http://steeeeeeepic.org/courses">
-https://regex101.com/r/gFBb8y/17
-<a.+href=[\"']https://|ftp://|([\w*\.\w*]+)['\"].*>
